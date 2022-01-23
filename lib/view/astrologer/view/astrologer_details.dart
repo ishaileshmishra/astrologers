05d6739118b2ff2dab75a961cbfea15c5add2a8b
Shailesh Mishra <mshaileshr@gmail.com>
2022-01-23 18:24:19 +0530

    file structure added

--- lib/view/astrologer/view/astrologer_details.dart
+++ lib/view/astrologer/view/astrologer_details.dart
@@ -1,7 +1,6 @@
 import 'package:astrologer/responsive_widgets/resp_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 
 import 'astrologer.dart';
 import '../widgets/appbar_details.dart';
